425d55f4b3791f4776926e04e6e599949af583a1
Kristofer Rogue <ice99dragon@gmail.com>
2024-09-01 23:52:43 +0700

    Adding file logger

--- logger.py
+++ logger.py
@@ -3,44 +3,50 @@ from typing import Callable
 
 
 class TkinterLogHandler(logging.Handler):
-    def __init__(self, log_callback: Callable[[str], None]) -> None:
-        """
-        Инициализация обработчика логов для Tkinter.
+    """
+    Кастомный обработчик для вывода логов в виджет Tkinter.
 
-        :param log_callback: Функция обратного вызова для передачи сообщений в интерфейс.
-        """
+    :param log_callback: Функция, которая принимает строку (сообщение) для вывода в интерфейс.
+    """
+
+    def __init__(self, log_callback: Callable[[str], None]) -> None:
         super().__init__()
         self.log_callback = log_callback
 
     def emit(self, record: logging.LogRecord) -> None:
         """
-        Передача лог-сообщений через функцию обратного вызова.
+        Метод обработки логов для вывода в интерфейс.
 
-        :param record: Запись логов.
+        :param record: Лог-запись, которая будет передана в интерфейс.
         """
         log_entry = self.format(record)
         self.log_callback(log_entry)
 
 
-def setup_logger(log_callback: Callable[[str], None]) -> logging.Logger:
+def setup_logger(log_callback: Callable[[str], None], log_file: str = "installer.log") -> logging.Logger:
     """
-    Настройка логгера для приложения.
+    Настройка логгера с обработчиками для интерфейса Tkinter и файла.
 
-    :param log_callback: Функция обратного вызова для логов.
+    :param log_callback: Функция для вывода логов в интерфейс.
+    :param log_file: Путь к файлу для записи логов.
     :return: Настроенный логгер.
     """
     logger = logging.getLogger("InstallerLogger")
-    logger.setLevel(logging.DEBUG)  # Устанавливаем уровень логирования
+    logger.setLevel(logging.DEBUG)
 
-    # Создаем обработчик для интерфейса
+    # Обработчик для Tkinter
     tk_handler = TkinterLogHandler(log_callback)
     tk_handler.setLevel(logging.DEBUG)
+    simple_formatter = logging.Formatter('%(message)s')
+    tk_handler.setFormatter(simple_formatter)
 
-    # Форматирование логов
-    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-    tk_handler.setFormatter(formatter)
+    # Обработчик для файла
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setLevel(logging.DEBUG)
+    detailed_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(detailed_formatter)
 
-    # Добавляем обработчик в логгер
     logger.addHandler(tk_handler)
+    logger.addHandler(file_handler)
 
     return logger
